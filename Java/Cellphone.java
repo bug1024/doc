af7c412cdd478587d4238bc597aa3a0ab125b9f1
bug1024 <bug1024.com@gmail.com>
2017-03-01 22:28:45 +0800

    java demo

--- Java/Cellphone.java
+++ Java/Cellphone.java
@@ -1,12 +1,37 @@
 
 public class Cellphone extends Telephone {
 
+    String brand;
+
+    public Cellphone(String brandName) {
+        brand = brandName;
+    }
+
     public void call() {
         System.out.println("call");
     }
 
     public void msg() {
         System.out.println("msg");
+    }
+
+    public void msg(String content) {
+        System.out.println("msg:" + content);
+    }
+
+    public static void main(String[] args) {
+        Cellphone phone = new Cellphone("iPhone");
+        System.out.println(phone.brand);
+        phone.call();
+        phone.msg();
+        phone.msg("Are you OK?");
+
+        int a = 12;
+        if (true) {
+            a = 123;
+        }
+
+        System.out.println(a);
     }
 
 }
