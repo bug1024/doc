cbb3a958febc7df84b9d7a2da3cf48a862f3567a
bug1024 <bug1024.com@gmail.com>
2017-07-24 23:26:17 +0800

    c

--- C/test.c
+++ C/test.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 #define ONE 1
 #define TWO 2
+
+void reverse(char str[]) {
+    int c, i, j;
+    for (i = 0, j = strlen(str) - 1; i < j ; i++, j--) {
+        c = str[i];
+        str[i] = str[j];
+        str[j] = c;
+    }
+}
 
 int main() {
     int a = ONE;
@@ -10,11 +20,22 @@ int main() {
     int b = 0;
     int c;
     while ((c = getchar()) != EOF) {
-        // '\n'是单字符，是一个整型数，而"\n"是包含1 个字符的字符串常量
+        // '\n'是单字符，是一个整型数，而"\n"是包含1 个字符以及1个结束符'\n'的字符数组
         if (c == '\n') {
             b++;
         }
     }
 
+    // 枚举变量，默认第一个值为0，第二个值为1
+    enum boolean {NO, YES};
+
+    // const限定符
+    const double e = 2.71828;
+
+    char str[] = "fork";
+    reverse(str);
+
     printf("%d\n", b);
+    printf("%s\n", str);
 }
+
